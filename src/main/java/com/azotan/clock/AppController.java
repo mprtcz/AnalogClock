48792603d1a20a73e423f54488ecf607cd11a083
Mordin <azet89@gmail.com>
2016-07-10 22:28:57 +0200

    UI polishing

--- src/main/java/com/azotan/clock/AppController.java
+++ src/main/java/com/azotan/clock/AppController.java
@@ -2,7 +2,6 @@ package com.azotan.clock;
 
 import javafx.application.Platform;
 import javafx.concurrent.Task;
-import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.shape.Line;
 import javafx.scene.transform.Rotate;
@@ -16,23 +15,32 @@ public class AppController {
 
     public Line secondsLine, minutesLine, hoursLine;
     public Button button;
-    int hour, minute, second;
+    private int hour, minute, second;
+    private boolean isRunning = false;
 
-    public void buttonClicked(ActionEvent event) throws InterruptedException {
+    public void buttonClicked() throws InterruptedException {
 
-        button.setDisable(true);
+        //button.setDisable(true);
+        isRunning = !isRunning;
+        if(isRunning){
+            button.setText("Stop Animation");
+        } else {
+            button.setText("Start Animation");
+        }
 
         Task task = new Task<Void>() {
 
             Rotate rotSec = new Rotate(0, secondsLine.getEndX(), 0, 0, Rotate.Z_AXIS);
             Rotate rotMin = new Rotate(0, minutesLine.getEndX(), 0, 0, Rotate.Z_AXIS);
             Rotate rotHr = new Rotate(0, hoursLine.getEndX(), 0, 0, Rotate.Z_AXIS);
+
+
             Date d = new Date();
 
             @Override
             public Void call() throws Exception {
                 System.out.println("Started...");
-                while(true) {
+                while(isRunning) {
                     Platform.runLater(() -> {
 
                         d.setTime(System.currentTimeMillis());
@@ -48,6 +56,7 @@ public class AppController {
                         rotMin.setAngle(6*minute);
                         rotHr.setAngle(30*hour + 0.5*minute);
 
+
                         secondsLine.getTransforms().clear();
                         minutesLine.getTransforms().clear();
                         hoursLine.getTransforms().clear();
@@ -55,9 +64,11 @@ public class AppController {
                         secondsLine.getTransforms().add(rotSec);
                         minutesLine.getTransforms().add(rotMin);
                         hoursLine.getTransforms().add(rotHr);
+
                     });
-                    Thread.sleep(100);
+                    Thread.sleep(10);
                 }
+                return null;
             }
         };
 
